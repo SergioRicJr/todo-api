ed4771a2ceded5a2e2f496638dd1c17c4c00a979
Sergio <sergioricjr7@gmail.com>
2023-12-22 11:31:16 -0300

    feat(urls): custom views of token added to the paths

--- core/urls.py
+++ core/urls.py
@@ -1,35 +1,17 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from rest_framework_simplejwt.views import TokenBlacklistView
 from todo.urls.userUrls import user_urls
 from todo.urls.taskUrls import task_urls
 from todo.urls.taskTypeUrls import task_type_urls
+from todo.views.auth.customTokenObtainPairView import CustomTokenObtainPairView
+from todo.views.auth.customTokenBlacklistView import CustomTokenBlacklistView
+from todo.views.auth.customTokenRefreshView import CustomTokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("auth/login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/blacklist/", CustomTokenBlacklistView.as_view(), name="token_blacklist"),
     path("users/", include(user_urls)),
     path("tasks/", include(task_urls)),
     path("task_type/", include(task_type_urls)),
