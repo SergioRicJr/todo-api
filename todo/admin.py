06ae9870c2dc7ff2d1c3656d005fbeac760a6336
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-12-22 17:13:48 -0300

    feat(admin): models of user, task and taskType added to django admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from todo.models.userModel import User
+from todo.models.taskModel import Task
+from todo.models.taskTypeModel import TaskType
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(TaskType)
