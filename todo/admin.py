9e93cf3e08a2a598fa7c1e74b64a2e5db86e30a7
SergioNascimento07 <sergioricjr7@gmail.com>
2024-01-04 17:29:19 -0300

    feat(admin): configuration of userAdmin, taskAdmin and taskTypeAdmin were created and applied

--- todo/admin.py
+++ todo/admin.py
@@ -3,6 +3,43 @@ from todo.models.userModel import User
 from todo.models.taskModel import Task
 from todo.models.taskTypeModel import TaskType
 
-admin.site.register(User)
-admin.site.register(Task)
-admin.site.register(TaskType)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name", "username"]
+    list_filter = ["is_active", "is_staff", "is_superuser", "is_deleted"]
+    list_display = [
+        "id",
+        "name",
+        "username",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "is_deleted",
+        "password",
+    ]
+
+
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ["title", "user"]
+    list_filter = ["completed", "user"]
+    list_display = [
+        "id",
+        "title",
+        "description",
+        "due_date",
+        "completed",
+        "user",
+        "task_type",
+    ]
+
+
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ["name", "user"]
+    list_filter = ["user"]
+    list_display = ["id", "name", "description", "user"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskType, TaskTypeAdmin)
