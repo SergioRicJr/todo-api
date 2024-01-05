3cba6c9c0168ba5860b0e9dce183a741fccd0783
SergioNascimento07 <sergioricjr7@gmail.com>
2024-01-05 17:18:42 -0300

    feat(views): filter and search added to the list view of task

--- todo/views/task/listTaskView.py
+++ todo/views/task/listTaskView.py
@@ -4,9 +4,33 @@ from rest_framework.response import Response
 from todo.serializers.taskSerializer import TaskSerializer
 from todo.models.taskModel import Task
 from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework import filters
 
 
 class ListTaskView(viewsets.ViewSet):
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+
+    ordering_fields = ["title", "id", "due_date"]
+
+    search_fields = ["title"]
+
+    filterset_fields = {
+        "user": ["exact"], 
+        "completed": ["exact"],
+        "task_type": ["exact"],
+        "due_date": ["exact", "in", "range", "lt", "lte", "gt", "gte"],
+    }
+
+    def filter_queryset(self, queryset):
+        for backend in self.filter_backends:
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
+
     def list(self, request):
         try:
             user = request.user
