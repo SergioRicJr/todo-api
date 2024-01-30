2046ac98c8d9e5ec8b50b045067995aee1f10035
SergioNascimento07 <sergioricjr7@gmail.com>
2024-01-30 20:57:53 -0300

    feat(core): observabilityMiddleware added to the settings.py

--- main_app/core/settings.py
+++ main_app/core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "todo.middlewares.observabilityMiddleware.ObservabilityMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
