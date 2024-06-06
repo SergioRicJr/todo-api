58401f6c31d004e8da5c599fe303bc1aa440a1dd
Sergio <sergioricjr7@gmail.com>
2024-06-06 02:25:03 -0300

    feat: taskType model updated to use double primary key and migration generated

--- main_app/todo/models/taskTypeModel.py
+++ main_app/todo/models/taskTypeModel.py
@@ -1,10 +1,17 @@
 from django.db import models
 from .userModel import User
 
+
 class TaskType(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_types") 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_types")
+
+    class Meta:
+        unique_together = (
+            "name",
+            "user",
+        )
 
     def __str__(self) -> str:
         return self.name
