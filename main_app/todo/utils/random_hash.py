aa19a5c1ce598e1a3f571505743f3328f61b58f3
Sergio <sergioricjr7@gmail.com>
2024-06-28 10:14:18 -0300

    feat(utils): mak_random_hash function changed to encapsulate secrets.token_hex()

--- main_app/todo/utils/random_hash.py
+++ main_app/todo/utils/random_hash.py
@@ -1,9 +1,4 @@
-import hashlib
-import random
-import string
+import secrets
 
 def make_random_hash(size=32):
-    caracteres = string.ascii_letters + string.digits
-    random_hash = ''.join(random.choice(caracteres) for _ in range(size))
-    random_hash = hashlib.sha256(random_hash.encode()).hexdigest()
-    return random_hash
+    return secrets.token_hex(size)
